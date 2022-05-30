b52ccdc8962194ac48f98991694efbb7cb4b0ca2
Antony Lo <mrmikadox@gmail.com>
2022-05-30 14:46:47 +0800

    working on game.js

--- frontend/game.js
+++ frontend/game.js
@@ -43,4 +43,51 @@ let questions = [
     choice4: '30% - 40%',
     answer: 1, // referring to the index of the answer
   },
-]
+];
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+const startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+const getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score);
+
+    return window.location.assign('/end.html');
+  }
+
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+
+    let classToApply = selectedAnswer == currentQuestion.answer ? 
+  })
+})

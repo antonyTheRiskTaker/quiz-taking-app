802085d5cd52ebd61ecba56129129c233a7afe48
Antony Lo <mrmikadox@gmail.com>
2022-05-30 14:16:51 +0800

    add hard-coded questions

--- frontend/game.js
+++ frontend/game.js
@@ -30,17 +30,17 @@ let questions = [
   {
     question: 'What percentage of American adults believe that chocolate milk comes from brown cows?',
     choice1: '20%',
-    choice2: '4',
-    choice3: '21',
-    choice4: '17',
-    answer: 2, // referring to the index of the answer?
+    choice2: '18%',
+    choice3: '7%',
+    choice4: '33%',
+    answer: 3, // referring to the index of the answer?
   },
   {
-    question: 'What is 2 + 2?',
-    choice1: '2',
-    choice2: '4',
-    choice3: '21',
-    choice4: '17',
-    answer: 2, // referring to the index of the answer?
+    question: 'Approximately what percent of U.S. power outages are caused by squirrels?',
+    choice1: '10% - 20%',
+    choice2: '5% - 10%',
+    choice3: '15% - 20%',
+    choice4: '30% - 40%',
+    answer: 1, // referring to the index of the answer?
   },
 ]
